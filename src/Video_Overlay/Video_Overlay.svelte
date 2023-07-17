08e4b54ccaac28f0db15de0eafddc1b38a4e0319
Kai <kai@kana.jetzt>
2023-07-17 15:33:12 +0200

    feat: :sparkles: added icons and translaton files

--- src/Video_Overlay/Video_Overlay.svelte
+++ src/Video_Overlay/Video_Overlay.svelte
@@ -1,12 +1,14 @@
 <script>
-import { onMount } from "svelte";
+// import { onMount } from "svelte";
 
-let id = undefined;
+// let id = undefined;
 
-onMount(() => {
-		window.Twitch.ext.onAuthorized(function () {
-				const config = window.Twitch.ext.configuration.broadcaster.content;
-				id = new URL(config).pathname.split("/")[2];
-		});
-});
+// onMount(() => {
+// 		window.Twitch.ext.onAuthorized(function () {
+// 				const config = window.Twitch.ext.configuration.broadcaster.content;
+// 				id = new URL(config).pathname.split("/")[2];
+// 		});
+// });
 </script>
+
+<h1>HELLO WORLD!</h1>
