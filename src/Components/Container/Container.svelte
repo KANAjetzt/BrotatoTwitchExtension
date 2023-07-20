55a7459c0fb8ad2f314946012b5457a71007b96d
Kai <kai@kana.jetzt>
2023-07-20 09:33:37 +0200

    refactor: :recycle: using css animations instead of svelte transitions

--- src/Components/Container/Container.svelte
+++ src/Components/Container/Container.svelte
@@ -1,26 +1,23 @@
 <script>
-	import { statefulSwap } from "../../stores"
-  import { fly } from 'svelte/transition';
-  import { quintOut } from 'svelte/easing';
-	import BtnFold from "../Btn_Fold/Btn_Fold.svelte";
+	import { fly } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+	import BtnFold from '../Btn_Fold/Btn_Fold.svelte';
 
-	export let fold_direction
+	export let fold_direction;
 
-	// https://stackoverflow.com/a/65171909
-	const {onOutro, transitionTo, state} = statefulSwap("is_not_folded")
-
-	let is_folded = false
+	let is_folded = false;
+	let show_is_folded_btn = false;
 
 	function get_transition_values() {
 		switch (fold_direction) {
-			case "right":
-				return {x: 400, duration: 200, easing: quintOut}
+			case 'right':
+				return { x: 400, duration: 200, easing: quintOut };
 				break;
-			case "down":
-				return {y: 200, duration: 200, easing: quintOut}
+			case 'down':
+				return { y: 200, duration: 200, easing: quintOut };
 				break;
-			case "left_top":
-				return {x: -200, duration: 200, easing: quintOut}
+			case 'left_top':
+				return { x: -200, duration: 200, easing: quintOut };
 				break;
 
 			default:
@@ -29,39 +26,48 @@
 	}
 </script>
 
-{#if $state === "is_not_folded"}
-	<div
-		class="container"
-		in:fly|local={get_transition_values()}
-		out:fly|local={get_transition_values()}
-	>
+<div
+	class="container {is_folded ? `${fold_direction}_animate_out` : `${fold_direction}_animate_in`}"
+>
 	<slot />
-	</div>
+</div>
+<div
+	class="btn_fold {fold_direction} {is_folded
+		? `${fold_direction}_animate_out`
+		: `${fold_direction}_animate_in`}"
+	on:animationend={() => {
+		if (is_folded) {
+			show_is_folded_btn = true;
+		}
+	}}
+>
+	<BtnFold
+		{fold_direction}
+		{is_folded}
+		on:click={(e) => {
+			is_folded = !is_folded;
+		}}
+	/>
+</div>
+
+{#if show_is_folded_btn}
 	<div
 		class="btn_fold {fold_direction}"
 		in:fly|local={get_transition_values()}
 		out:fly|local={get_transition_values()}
-		on:outroend={onOutro}
+		on:outroend={() => {
+			is_folded = false;
+		}}
 	>
-		<BtnFold {fold_direction} {is_folded} on:click={e =>  {
-				is_folded = !is_folded
-				transitionTo("is_folded")
-			}} />
-	</div>
-{:else if $state === "is_folded"}
-	<div
-		class="btn_fold {fold_direction}"
-		in:fly|local={get_transition_values()}
-		out:fly|local={get_transition_values()}
-		on:outroend={onOutro}
-	>
-		<BtnFold {fold_direction} {is_folded} on:click={e =>  {
-				transitionTo("is_not_folded")
-				is_folded = !is_folded
-			}} />
+		<BtnFold
+			{fold_direction}
+			{is_folded}
+			on:click={(e) => {
+				show_is_folded_btn = false;
+			}}
+		/>
 	</div>
 {/if}
-
 
 <style>
 	.container {
@@ -80,17 +86,41 @@
 
 	.right {
 		right: 0rem;
-    bottom: -2rem;
+		bottom: -2rem;
+	}
+
+	.right_animate_in {
+		animation: slide_in_right 0.2s ease-in-out forwards;
+	}
+
+	.right_animate_out {
+		animation: slide_out_right 0.2s ease-in-out forwards;
+	}
+
+	.down_animate_in {
+		animation: slide_in_down 0.2s ease-in-out forwards;
+	}
+
+	.down_animate_out {
+		animation: slide_out_down 0.2s ease-in-out forwards;
+	}
+
+	.left_top_animate_in {
+		animation: slide_in_left 0.2s ease-in-out forwards;
+	}
+
+	.left_top_animate_out {
+		animation: slide_out_left 0.2s ease-in-out forwards;
 	}
 
 	.left {
 		right: 0rem;
-    bottom: -2rem;
+		bottom: -2rem;
 	}
 
 	.down {
 		right: -2rem;
-    bottom: 0;
+		bottom: 0;
 	}
 
 	.left_top {
