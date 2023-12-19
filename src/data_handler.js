179a6cef13e18e8dd4839522cc7b16b640ca892a
Kai <kai@kana.jetzt>
2023-12-19 21:49:48 +0100

    refactor: :recycle: added catch up

--- src/data_handler.js
+++ src/data_handler.js
@@ -1,37 +1,44 @@
-let temp_image_base64 = [];
+let temp_image_base64 = {};
 
 let game_data = {
 	stats_primary: {},
 	stats_secondary: {},
 	items: [],
 	weapons: [],
-	stored_images: {}
+	stored_images: {},
+	handled_actions: {}
 };
 
 export function data_handler(data) {
 	data.forEach((update_data) => {
-		const { action, data } = update_data;
+		const { id, action} = update_data;
+
+		// Skip if the action has been handled in the past
+		if(id !== "" && Object.hasOwn(game_data.handled_actions, id)){
+			return
+		}
+
 		switch (action) {
 			case 'item_added':
-				item_add(data);
+				item_add(update_data);
 				break;
 			case 'weapon_added':
-				weapon_added(data);
+				weapon_added(update_data);
 				break;
 			case 'weapon_removed':
-				weapon_removed(data);
+				weapon_removed(update_data);
 				break;
 			case 'stats_update':
-				stats_update(data);
+				stats_update(update_data);
 				break;
 			case 'image_upload':
-				image_upload(data);
+				image_upload(update_data);
 				break;
 			case 'image_upload_start':
-				image_upload_start(data);
+				image_upload_start(update_data);
 				break;
 			case 'image_upload_end':
-				image_upload_end(data);
+				image_upload_end(update_data);
 				break;
 			case 'clear_all':
 				clear_all();
@@ -45,15 +52,23 @@ export function data_handler(data) {
 	return game_data;
 }
 
-function item_add(data) {
+function item_add(update_data) {
+	const {id, data} = update_data;
+
 	game_data?.items.push(data);
+	game_data.handled_actions[id] = data
 }
 
-function weapon_added(data) {
+function weapon_added(update_data) {
+	const { id, data} = update_data;
+
 	game_data?.weapons.push(data);
+	game_data.handled_actions[id] = data
 }
 
-function weapon_removed(data) {
+function weapon_removed(update_data) {
+	const { id, data} = update_data;
+
 	const index_to_remove = game_data.weapons.findIndex((weapon_data) => {
 		if (weapon_data.tier === data.tier && weapon_data.id === data.id) {
 			return true;
@@ -62,9 +77,12 @@ function weapon_removed(data) {
 	});
 
 	game_data.weapons.splice(index_to_remove, 1);
+	game_data.handled_actions[id] = data
 }
 
-function stats_update(data) {
+function stats_update(update_data) {
+	const { id, data} = update_data;
+
 	for (const [key, value] of Object.entries(data)) {
 		if (key.startsWith('stat')) {
 			game_data.stats_primary[key] = value;
@@ -72,21 +90,49 @@ function stats_update(data) {
 			game_data.stats_secondary[key] = value;
 		}
 	}
+	game_data.handled_actions[id] = data
 }
 
-function image_upload(data) {
-	temp_image_base64.push(data.base64_chunks);
+function image_upload(update_data) {
+	const { id, data} = update_data;
+
+	// Check if the image upload has started
+	if(!Object.hasOwn(temp_image_base64, data.item_id)){
+		return
+	}
+
+	temp_image_base64[data.item_id].push(data.base64_chunks);
+	game_data.handled_actions[id] = data
 }
 
-function image_upload_start(data) {
-	temp_image_base64.push(data.base64_chunks);
+function image_upload_start(update_data) {
+	const { id, data} = update_data;
+
+	temp_image_base64[data.item_id] = []
+	temp_image_base64[data.item_id].push(data.base64_chunks);
+	game_data.handled_actions[id] = data
 }
 
-function image_upload_end(data) {
+function image_upload_end(update_data) {
+	const { id, data} = update_data;
+
 	const { item_id, base64_chunks } = data;
-	temp_image_base64.push(base64_chunks);
-	game_data.stored_images[item_id] = temp_image_base64.join('');
-	temp_image_base64 = [];
+
+	// Check if the image upload has started
+	if(!Object.hasOwn(temp_image_base64, item_id)){
+		return
+	}
+
+	// Add the last image chunk
+	temp_image_base64[item_id].push(base64_chunks);
+
+	// Store the full base64 string in game_data
+	game_data.stored_images[item_id] = temp_image_base64[item_id].join('');
+
+	// Clear the temp image entry
+	delete temp_image_base64[item_id]
+
+	game_data.handled_actions[id] = data
 	console.log(game_data);
 }
 
@@ -96,6 +142,7 @@ function clear_all() {
 		stats_secondary: {},
 		items: [],
 		weapons: [],
-		stored_images: {}
+		stored_images: {},
+		handled_actions: {}
 	};
 }
