00dc6286ae4a16f1fd4417f812de63b6d7d7eddc
Kai <kai@kana.jetzt>
2024-12-15 10:53:59 +0100

    feat: :sparkles: config page

--- src/Config/Config.svelte
+++ src/Config/Config.svelte
@@ -1,1 +1,13 @@
-<h1>HELLO WORLD!</h1>
+<h1>Configuration</h1>
+
+<p>
+	No additional configuration is needed here. <br /> Make sure to download the backend service mod from
+	the Steam Workshop.
+</p>
+<br />
+<a
+	target="_blank"
+	rel="noopener noreferrer"
+	href="https://steamcommunity.com/sharedfiles/filedetails/?id=3384559178"
+	>Twitch Extension - Item and Stats Overlay</a
+>
